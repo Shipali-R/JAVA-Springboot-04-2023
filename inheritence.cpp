5879328608acb74e84fb2c5d7fd96f964e391448
Shipali-R <shipalirana853@gmail.com>
2023-04-18 20:43:38 +0530

    Destructor is C

--- inheritence.cpp
+++ inheritence.cpp
@@ -2,10 +2,11 @@
 #include<iostream>
 
 class Parent{
-    private:
+    public:
         //Accisible only within the class as the access specifier is private.
         int a;
         int b;
+        int *pt;
     public:
     //Parameterized Constructor
     Parent(int p,int q){
@@ -20,7 +21,8 @@ class Parent{
         std::cout<<"I am a destructor of the Parent \n";
     }
     void show(){
-            std::cout<<"I am show of the Parent \n" <<a <<b << "\n";
+            std::cout<<"I am show of the Parent and going to occupy 100 bytes of space \n";
+            pt = (int*) malloc(100);
         }
 
         
@@ -39,9 +41,14 @@ class Child : public Parent{
         std::cout<<"I am a parametrized constructor of Child \n";
     }
     ~Child(){
-        std::cout<<"I am a destructor of the Child \n";
-    }
-
+        free(pt); 
+        std::cout<<"I am a destructor of the Child \n"; 
+        //Destructor needed to avoid memory leak.
+        //As the heap memory will not be deleted once the main function ends.
+        //It frees only the stack space.
+        //Destructor is used to de allocate the resources occupied by the object when not needed.
+        //Destructor does not take any arguments.
+    };
 };
 
 int main(){
@@ -51,6 +58,7 @@ int main(){
     
     Child c;
     Child c2(100,200);
+    c.show();
     // p1.show();
     // p3.show();
     
