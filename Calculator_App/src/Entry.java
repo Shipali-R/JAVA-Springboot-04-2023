b3783989474d2012a122fc2b94d946914cc7c835
Shipali-R <shipali.rana@excellarate.com>
2023-05-15 10:53:08 +0530

    Claculator App - Day 5 commits

--- Calculator_App/src/Entry.java
+++ Calculator_App/src/Entry.java
@@ -1,19 +1,53 @@
+import java.io.IOException;
 
 public class Entry {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		System.out.println("My Calculator\n");
+		System.out.println("Please enter the first number");
+		int number1 = BasicInput.readInteger();
 		
-		int a = 100;
-		int b = 20;
+		System.out.println("Please enter the second number");
+		int number2 = BasicInput.readInteger();
 		
-		Maths m= new Maths();
-		m.add(a,b);
-		m.subtract(a,b);
-		m.multiply(a,b);
-		m.division(a, b);
-		m.square(a);
-		m.cube(a);
+		Maths m = new Maths();
+		m.add(number1, number2);
+		m.multiply(number1, number2);
 		
+//		byte b[] = new byte[255];
+//		System.out.println("Please enter 1st number");
+//		int length = System.in.read(b);
+//		byte bt[] = new byte[length-2];
+//		System.arraycopy(b, 0, bt, 0, length-2); //Function called without creating object(static function)
+//		String s = new String(bt);
+//		
+//		int number = Integer.parseInt(s); //Wraper class
+//		System.out.println(number);
+		//System.out.println("The number is "+ s);		
+		//System.out.println("The length is "+s.length());		
+		
+//		The second way to find out length of the string is through read function.
+//		int length = System.in.read(b);
+//		System.out.println("The length of the string is "+length);
+//		System.out.println(b);		
+		
+//		One way of finding length of string. Its gives us value as 255.
+//		System.in.read(b);
+//		System.out.println(b);
+//		//Converting the byte input to string.
+//		String s = new String(b);
+//		System.out.println("The number is "+ s);		
+//		System.out.println("The length of the string is "+s.length());
+
+//		System.out.println("My Calculator\n");		
+//		int a = 100;
+//		int b = 20;
+//		Maths m= new Maths();
+//		m.add(a,b);
+//		m.subtract(a,b);
+//		m.multiply(a,b);
+//		m.division(a, b);
+//		m.square(a);
+//		m.cube(a);
+//		
 	}
 }
